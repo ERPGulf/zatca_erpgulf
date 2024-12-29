15c2cd778a73792ad6cefa04e86f34b8c6c0b601
HusnaMa <husna@htsqatar.com>
2024-12-29 16:04:40 +0300

    Fixes on compliance

--- zatca_erpgulf/zatca_erpgulf/sign_invoice_first.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice_first.py
@@ -1058,8 +1058,8 @@ def compliance_api_call(uuid1, encoded_hash, signed_xmlfile_name, company_abbr):
             data=payload,
             timeout=30,
         )
-        frappe.throw("after call")
-        # frappe.throw(response.text)
+        # frappe.throw("after call")
+        frappe.throw(response.text)
         if response.status_code != 200:
             frappe.throw(f"Error in compliance: {response.text}")
         if response.status_code != 202:
