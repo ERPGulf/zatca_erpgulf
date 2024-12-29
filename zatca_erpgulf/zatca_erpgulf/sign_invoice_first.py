0c52b564a31dd04d9a807a4f9ee761a09c5e2580
HusnaMa <husna@htsqatar.com>
2024-12-29 16:16:29 +0300

    Fixes on compliance

--- zatca_erpgulf/zatca_erpgulf/sign_invoice_first.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice_first.py
@@ -1032,7 +1032,7 @@ def compliance_api_call(uuid1, encoded_hash, signed_xmlfile_name, company_abbr):
         frappe.throw(f"Company with abbreviation {company_abbr} not found.")
 
     company_doc = frappe.get_doc("Company", company_name)
-    frappe.throw(uuid1)
+    frappe.throw(encoded_hash)
     payload = json.dumps(
         {
             "invoiceHash": encoded_hash,
