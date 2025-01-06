27ac284c91de67885267276c4fd881447fbc9315
HusnaMa <husna@htsqatar.com>
2025-01-06 12:01:03 +0300

    version updated

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-        install_requires = f.read().strip().split('\n')
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in zatca2024/__init__.py
 from zatca_erpgulf import __version__ as version
 
 setup(
-        name='zatca_erpgulf',
-        version=version,
-        description='zatca_erpgulf',
-        author='Husna',
-        author_email='support@erpgulf.com',
-        packages=find_packages(),
-        zip_safe=False,
-        include_package_data=True,
-        install_requires=install_requires
+    name="zatca_erpgulf",
+    version=version,
+    description="zatca_erpgulf",
+    author="Husna",
+    author_email="support@erpgulf.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
 )
