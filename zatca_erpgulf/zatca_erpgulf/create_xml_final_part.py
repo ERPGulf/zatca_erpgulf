4ab9a215ab8e1d37cc9fa1d0515a5867389226be
HusnaMa <husna@htsqatar.com>
2025-01-08 09:38:34 +0300

    version updated

--- zatca_erpgulf/zatca_erpgulf/create_xml_final_part.py
+++ zatca_erpgulf/zatca_erpgulf/create_xml_final_part.py
@@ -14,6 +14,8 @@ from zatca_erpgulf.zatca_erpgulf.xml_tax_data import (
 ITEM_TAX_TEMPLATE = "Item Tax Template"
 CAC_TAX_TOTAL = "cac:TaxTotal"
 CBC_TAX_AMOUNT = "cbc:TaxAmount"
+CAC_TAX_SUBTOTAL = "cac:TaxSubtotal"
+CBC_TAXABLE_AMOUNT = "cbc:TaxableAmount"
 
 def tax_data_with_template_nominal(invoice, sales_invoice_doc):
     """
@@ -116,8 +118,8 @@ def tax_data_with_template_nominal(invoice, sales_invoice_doc):
 
         # Create XML elements for each ZATCA tax category
         for zatca_tax_category, totals in tax_category_totals.items():
-            cac_taxsubtotal = ET.SubElement(cac_taxtotal, "cac:TaxSubtotal")
-            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, "cbc:TaxableAmount")
+            cac_taxsubtotal = ET.SubElement(cac_taxtotal, CAC_TAX_SUBTOTAL)
+            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, CBC_TAXABLE_AMOUNT)
             cbc_taxableamount.set("currencyID", sales_invoice_doc.currency)
             cbc_taxableamount.text = str(abs(round(sales_invoice_doc.base_total, 2)))
 
@@ -169,8 +171,8 @@ def tax_data_with_template_nominal(invoice, sales_invoice_doc):
             cbc_taxscheme_id = ET.SubElement(cac_taxscheme, "cbc:ID")
             cbc_taxscheme_id.text = "VAT"
 
-        cac_taxsubtotal_2 = ET.SubElement(cac_taxtotal, "cac:TaxSubtotal")
-        cbc_taxableamount_2 = ET.SubElement(cac_taxsubtotal_2, "cbc:TaxableAmount")
+        cac_taxsubtotal_2 = ET.SubElement(cac_taxtotal, CAC_TAX_SUBTOTAL)
+        cbc_taxableamount_2 = ET.SubElement(cac_taxsubtotal_2, CBC_TAXABLE_AMOUNT)
         cbc_taxableamount_2.set("currencyID", "SAR")
         cbc_taxableamount_2.text = str(-round(abs(sales_invoice_doc.total), 2))
 
@@ -319,8 +321,8 @@ def tax_data_nominal(invoice, sales_invoice_doc):
             )
 
             # Tax Subtotal
-            cac_taxsubtotal = ET.SubElement(cac_taxtotal, "cac:TaxSubtotal")
-            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, "cbc:TaxableAmount")
+            cac_taxsubtotal = ET.SubElement(cac_taxtotal, CAC_TAX_SUBTOTAL)
+            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, CBC_TAXABLE_AMOUNT)
             cbc_taxableamount.set("currencyID", sales_invoice_doc.currency)
             if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
                 cbc_taxableamount.text = str(
@@ -378,8 +380,8 @@ def tax_data_nominal(invoice, sales_invoice_doc):
             cbc_taxamount_usd.text = str(round(tax_amount_without_retention, 2))
 
             # Tax Subtotal
-            cac_taxsubtotal = ET.SubElement(cac_taxtotal, "cac:TaxSubtotal")
-            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, "cbc:TaxableAmount")
+            cac_taxsubtotal = ET.SubElement(cac_taxtotal, CAC_TAX_SUBTOTAL)
+            cbc_taxableamount = ET.SubElement(cac_taxsubtotal, CBC_TAXABLE_AMOUNT)
             cbc_taxableamount.set("currencyID", sales_invoice_doc.currency)
             cbc_taxableamount.text = str(abs(round(taxable_amount_1, 2)))
 
@@ -443,8 +445,8 @@ def tax_data_nominal(invoice, sales_invoice_doc):
                 taxable_amount = total_line_extension
             else:
                 taxable_amount = sales_invoice_doc.base_discount_amount
-        cac_taxsubtotal_2 = ET.SubElement(cac_taxtotal, "cac:TaxSubtotal")
-        cbc_taxableamount_2 = ET.SubElement(cac_taxsubtotal_2, "cbc:TaxableAmount")
+        cac_taxsubtotal_2 = ET.SubElement(cac_taxtotal, CAC_TAX_SUBTOTAL)
+        cbc_taxableamount_2 = ET.SubElement(cac_taxsubtotal_2, CBC_TAXABLE_AMOUNT)
         cbc_taxableamount_2.set("currencyID", "SAR")
         cbc_taxableamount_2.text = str(-round(taxable_amount, 2))
 
