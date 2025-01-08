4b1eb8a6d762aa23a8fc805681d6cc93c28b5b55
HusnaMa <husna@htsqatar.com>
2025-01-08 11:40:48 +0300

    version updated for xml tax data2

--- zatca_erpgulf/zatca_erpgulf/xml_tax_data.py
+++ zatca_erpgulf/zatca_erpgulf/xml_tax_data.py
@@ -7,6 +7,7 @@ import json
 import xml.etree.ElementTree as ET
 import frappe
 TAX_CALCULATION_ERROR = "Tax Calculation Error"
+CAC_TAX_TOTAL = "cac:TaxTotal"
 
 
 def get_exemption_reason_map():
@@ -99,7 +100,7 @@ def tax_data(invoice, sales_invoice_doc):
     try:
         # Handle SAR-specific logic
         if sales_invoice_doc.currency == "SAR":
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount_sar = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount_sar.set(
                 "currencyID", "SAR"
@@ -114,7 +115,7 @@ def tax_data(invoice, sales_invoice_doc):
             taxable_amount = sales_invoice_doc.base_total - sales_invoice_doc.get(
                 "base_discount_amount", 0.0
             )
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount.set("currencyID", sales_invoice_doc.currency)
             tax_amount_without_retention = round(
@@ -145,7 +146,7 @@ def tax_data(invoice, sales_invoice_doc):
 
         # Handle USD-specific logic
         else:
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount_usd_1 = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount_usd_1.set(
                 "currencyID", sales_invoice_doc.currency
@@ -163,7 +164,7 @@ def tax_data(invoice, sales_invoice_doc):
                 taxable_amount_1 * float(sales_invoice_doc.taxes[0].rate) / 100
             )
             cbc_taxamount_usd_1.text = str(round(tax_amount_without_retention, 2))
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount_usd = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount_usd.set(
                 "currencyID", sales_invoice_doc.currency
@@ -396,7 +397,7 @@ def tax_data_with_template(invoice, sales_invoice_doc):
 
         # For SAR currency
         if sales_invoice_doc.currency == "SAR":
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount_sar = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount_sar.set(
                 "currencyID", "SAR"
@@ -404,19 +405,19 @@ def tax_data_with_template(invoice, sales_invoice_doc):
             tax_amount_without_retention_sar = round(abs(total_tax), 2)
             cbc_taxamount_sar.text = str(tax_amount_without_retention_sar)
 
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount.set("currencyID", sales_invoice_doc.currency)
             tax_amount_without_retention = round(abs(total_tax), 2)
             cbc_taxamount.text = str(tax_amount_without_retention)
         else:
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount_sar = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount_sar.set("currencyID", sales_invoice_doc.currency)
             tax_amount_without_retention_sar = round(abs(total_tax), 2)
             cbc_taxamount_sar.text = str(tax_amount_without_retention_sar)
 
-            cac_taxtotal = ET.SubElement(invoice, "cac:TaxTotal")
+            cac_taxtotal = ET.SubElement(invoice, CAC_TAX_TOTAL)
             cbc_taxamount = ET.SubElement(cac_taxtotal, "cbc:TaxAmount")
             cbc_taxamount.set("currencyID", sales_invoice_doc.currency)
             tax_amount_without_retention = round(abs(total_tax), 2)
