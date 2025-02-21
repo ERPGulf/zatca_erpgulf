1654c8fe08b3a9db966367f07915e8a25c0e6663
HusnaMa <husna@htsqatar.com>
2025-02-21 12:32:30 +0300

    removed custom doctype

--- zatca_erpgulf/zatca_erpgulf/pos_sign.py
+++ zatca_erpgulf/zatca_erpgulf/pos_sign.py
@@ -614,6 +614,7 @@ def zatca_call(
 
         if compliance_type == "0":
             if customer_doc.custom_b2c == 1:
+                attach_qr_image(qrcodeb64, pos_invoice_doc)
                 reporting_api(
                     uuid1,
                     encoded_hash,
@@ -621,7 +622,7 @@ def zatca_call(
                     invoice_number,
                     pos_invoice_doc,
                 )
-                attach_qr_image(qrcodeb64, pos_invoice_doc)
+
             else:
                 clearance_api(
                     uuid1,
