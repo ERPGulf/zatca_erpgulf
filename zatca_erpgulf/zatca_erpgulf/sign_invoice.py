ea6943945cb1b8c51336dfa070a4249c6fc73a61
HusnaMa <husna@htsqatar.com>
2025-09-01 15:46:04 +0300

    the feature tax id removed for b2b customer when there is customer number

--- zatca_erpgulf/zatca_erpgulf/sign_invoice.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice.py
@@ -1223,7 +1223,7 @@ def zatca_background(invoice_number, source_doc, bypass_background_check=False):
                         )
                     )
             # if customer_doc.custom_b2c != 1:
-            if address and address.country == SAUDI_ARABIA:
+            if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
                 if not customer_doc.tax_id:
                     frappe.throw(
                         _(
@@ -1657,7 +1657,8 @@ def zatca_background_on_submit(doc, _method=None, bypass_background_check=False)
                             "As per ZATCA regulations, Pincode must be exactly 5 digits in customer address."
                         )
                     )
-            if address and address.country == SAUDI_ARABIA:
+            if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
+            
                 if not customer_doc.tax_id:
                     frappe.throw(
                         _(
