957985adec50609e9690c93bcd63c5b3d777e8dd
HusnaMa <husna@htsqatar.com>
2025-09-04 17:04:57 +0300

    WIP: my changes

--- zatca_erpgulf/zatca_erpgulf/sign_invoice.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice.py
@@ -481,7 +481,7 @@ def reporting_api(
                     error_log()
                 
             except (ValueError, TypeError, KeyError, frappe.ValidationError) as e:
-                frappe.throw(_(f"Error in reporting API-2 : {str(e)}"))
+                frappe.throw(_(f"Error in reporting API-2 original reporting: {str(e)}"))
 
     except (ValueError, TypeError, KeyError, frappe.ValidationError) as e:
         invoice_doc = frappe.get_doc("Sales Invoice", invoice_number)
@@ -1259,8 +1259,8 @@ def zatca_background(invoice_number, source_doc, bypass_background_check=False):
                         )
                     )
             # if customer_doc.custom_b2c != 1:
-            # if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
-            if address and address.country == SAUDI_ARABIA:
+            if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
+            # if address and address.country == SAUDI_ARABIA:
                 if not customer_doc.tax_id:
                     frappe.throw(
                         _(
@@ -1694,8 +1694,8 @@ def zatca_background_on_submit(doc, _method=None, bypass_background_check=False)
                             "As per ZATCA regulations, Pincode must be exactly 5 digits in customer address."
                         )
                     )
-            # if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
-            if address and address.country == SAUDI_ARABIA:
+            if address and address.country == SAUDI_ARABIA and not customer_doc.custom_buyer_id:
+            # if address and address.country == SAUDI_ARABIA:
                 if not customer_doc.tax_id:
                     frappe.throw(
                         _(
