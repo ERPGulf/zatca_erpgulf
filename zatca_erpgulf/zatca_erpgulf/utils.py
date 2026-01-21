8f267404b6cbfaec529339d32f754ec1bf75f315
HusnaMa <husna@htsqatar.com>
2026-01-21 10:16:23 +0300

    the type added in fixtures

--- zatca_erpgulf/zatca_erpgulf/utils.py
+++ zatca_erpgulf/zatca_erpgulf/utils.py
@@ -20,3 +20,83 @@ def arabic_money_in_words(amount):
 
     except Exception:
         return ""
+
+
+def arabic_number(value):
+    if value in (None, ""):
+        return ""
+
+    arabic_digits = "٠١٢٣٤٥٦٧٨٩"
+    english_digits = "0123456789"
+
+    value = str(value)
+
+    # Replace decimal point with Arabic decimal
+    value = value.replace(".", "٫")
+
+    trans = str.maketrans(english_digits, arabic_digits)
+    return value.translate(trans)
+
+
+
+import frappe
+import qrcode
+from io import BytesIO
+from frappe.utils import now_datetime
+def generate_qr_and_attach_doctype(doctype, docname, url, file_field=None):
+    if not url:
+        frappe.throw("QR URL is required")
+
+    doc = frappe.get_doc(doctype, docname)
+
+    filename = f"{doctype}-{docname}-QR.png".replace(" ", "_")
+
+    existing = frappe.db.exists("File", {
+        "attached_to_doctype": doctype,
+        "attached_to_name": docname,
+        "file_name": filename
+    })
+
+    if existing:
+        return frappe.db.get_value("File", existing, "file_url")
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        box_size=12,
+        border=2,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    file_doc = frappe.get_doc({
+        "doctype": "File",
+        "file_name": filename,
+        "attached_to_doctype": doctype,
+        "attached_to_name": docname,
+        "content": buffer.getvalue(),
+        "is_private": 0
+    })
+    file_doc.insert(ignore_permissions=True)
+
+    if file_field and doc.meta.has_field(file_field):
+        doc.set(file_field, file_doc.file_url)
+        doc.save(ignore_permissions=True)
+
+    return file_doc.file_url
+
+
+@frappe.whitelist()
+def generate_qr_for_doc(doctype, docname, url, file_field=None):
+    return generate_qr_and_attach_doctype(
+        doctype=doctype,
+        docname=docname,
+        url=url,
+        file_field=file_field
+    )
