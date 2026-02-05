c1c4fd565db83b49ac17c8be0ba2d4d3dc35d146
HusnaMa <husna@htsqatar.com>
2026-02-05 11:28:15 +0300

    new push

--- zatca_erpgulf/hooks.py
+++ zatca_erpgulf/hooks.py
@@ -13,7 +13,7 @@ from . import __version__ as app_version
 add_to_apps_screen = [
     {
         "name": app_name,
-        "logo": "/assets/zatca_erpgulf/images/ERPGulf.png",
+        "logo": "/assets/zatca_erpgulf/images/erpgulf.svg",
         "title": app_title,
         "route": app_home,
         # "has_permission": "zatca_erpgulf.check_app_permission",
