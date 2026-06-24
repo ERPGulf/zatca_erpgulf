6922be1e9112e276bb4a78c1df1b0dcc335e9e1f
Harshafiroz <husna@htsqatar.com>
2026-06-24 08:23:30 +0300

    vat report for import and standard purchase updated

--- zatca_erpgulf/zatca_erpgulf/report/vat_report_zatca_on_sales_&_purchase/vat_report_zatca_on_sales_&_purchase.py
+++ zatca_erpgulf/zatca_erpgulf/report/vat_report_zatca_on_sales_&_purchase/vat_report_zatca_on_sales_&_purchase.py
@@ -171,239 +171,6 @@ def build_filters_sql(filters, table_alias="si"):
     return " AND ".join(conditions)
 
 
-# # =========================================================
-# # SALES VAT
-# # =========================================================
-# def get_sales_vat_totals_sql(filters):
-
-#     totals = {
-#         "Standard": {"amount": 0, "adjustment": 0, "vat": 0},
-#         "HealthcareEdu": {"amount": 0, "adjustment": 0, "vat": 0},
-#         "Zero Rated": {"amount": 0, "adjustment": 0, "vat": 0},
-#         "Exports": {"amount": 0, "adjustment": 0, "vat": 0},
-#         "Exempt": {"amount": 0, "adjustment": 0, "vat": 0},
-#     }
-
-#     where_clause = build_filters_sql(filters)
-
-#     query = f"""
-#         SELECT
-#             si.name AS invoice,
-#             si.is_return AS is_return,
-#             si.is_debit_note,
-#             si.grand_total AS grand_total,
-
-#             COALESCE((
-#                 SELECT SUM(stc.tax_amount)
-#                 FROM `tabSales Taxes and Charges` stc
-#                 INNER JOIN `tabAccount` acc
-#                     ON acc.name = stc.account_head
-#                 WHERE stc.parent = si.name
-#                 AND acc.account_type = 'Tax'
-#             ), 0) AS total_vat_amount,
-
-#             si.custom_zatca_tax_category AS invoice_zatca_cat,
-#             si.custom_exemption_reason_code AS invoice_exemption_code,
-#             si.custom_zatca_export_invoice AS invoice_export_flag,
-
-#             sii.name AS item_name,
-#             COALESCE(sii.amount, 0) AS item_amount,
-#             COALESCE(sii.net_amount, sii.amount, 0) AS item_net_amount,
-#             sii.item_tax_template AS item_tax_template,
-
-#             itt.custom_zatca_tax_category AS template_category,
-#             itt.custom_exemption_reason_code AS template_exemption_code,
-
-#             tax.tax_rate AS template_first_tax_rate
-
-#         FROM `tabSales Invoice` si
-
-#         LEFT JOIN `tabSales Invoice Item` sii
-#             ON sii.parent = si.name
-
-#         LEFT JOIN `tabItem Tax Template` itt
-#             ON itt.name = sii.item_tax_template
-
-#         LEFT JOIN `tabItem Tax Template Detail` tax
-#             ON tax.parent = itt.name
-#             AND tax.idx = 1
-
-#         WHERE {where_clause}
-#     """
-
-#     rows = frappe.db.sql(query, filters, as_dict=True)
-
-#     invoices = {}
-
-#     for r in rows:
-
-#         inv = r.get("invoice")
-
-#         if inv not in invoices:
-#             invoices[inv] = {
-#                 "is_return": bool(r.get("is_return")),
-#                 "is_debit_note": bool(r.get("is_debit_note")),
-#                 "grand_total": r.get("grand_total") or 0,
-#                 "total_vat_amount": r.get("total_vat_amount") or 0,
-#                 "custom_zatca_tax_category": r.get("invoice_zatca_cat"),
-#                 "custom_exemption_reason_code": r.get("invoice_exemption_code"),
-#                 "custom_zatca_export_invoice": r.get("invoice_export_flag") or 0,
-#                 "items": []
-#             }
-
-#         if r.get("item_name"):
-#             invoices[inv]["items"].append({
-#                 "amount": r.get("item_amount") or 0,
-#                 "net_amount": r.get("item_net_amount") or 0,
-#                 "item_tax_template": r.get("item_tax_template"),
-#                 "template_category": r.get("template_category"),
-#                 "template_exemption_code": r.get("template_exemption_code"),
-#                 "tax_rate": r.get("template_first_tax_rate") or 0,
-#             })
-
-#     for inv_doc in invoices.values():
-
-#         is_return = bool(inv_doc["is_return"])
-#         is_debit = bool(inv_doc["is_debit_note"])
-
-#         key = "adjustment" if (is_return or is_debit) else "amount"
-
-#         has_item_template = any(
-#             item.get("item_tax_template")
-#             for item in inv_doc["items"]
-#         )
-
-#         def calculate_item_vat(item):
-#             return (
-#                 (item.get("net_amount") or 0)
-#                 * (item.get("tax_rate") or 0)
-#                 / 100.0
-#             )
-
-#         signed_grand_total = inv_doc["grand_total"] or 0
-#         signed_vat = inv_doc["total_vat_amount"] or 0
-
-#         if is_return:
-#             signed_grand_total = -abs(signed_grand_total)
-#             signed_vat = -abs(signed_vat)
-
-#         elif is_debit:
-#             signed_grand_total = abs(signed_grand_total)
-#             signed_vat = abs(signed_vat)
-
-#         # STANDARD
-#         if inv_doc.get("custom_zatca_tax_category") == "Standard":
-
-#             totals["Standard"][key] += signed_grand_total
-
-#             if not has_item_template:
-#                 totals["Standard"]["vat"] += signed_vat
-
-#         else:
-
-#             for item in inv_doc["items"]:
-
-#                 if item.get("template_category") == "Standard":
-
-#                     item_amount = item.get("amount") or 0
-#                     item_vat = calculate_item_vat(item)
-
-#                     if is_return:
-#                         item_amount = -abs(item_amount)
-#                         item_vat = -abs(item_vat)
-
-#                     elif is_debit:
-#                         item_amount = abs(item_amount)
-#                         item_vat = abs(item_vat)
-
-#                     totals["Standard"][key] += item_amount
-#                     totals["Standard"]["vat"] += item_vat
-
-#         # ZERO RATED
-#         if inv_doc.get("custom_zatca_tax_category") == "Zero Rated":
-
-#             totals["Zero Rated"][key] += signed_grand_total
-
-#         else:
-
-#             for item in inv_doc["items"]:
-
-#                 if item.get("template_category") == "Zero Rated":
-
-#                     item_amount = item.get("amount") or 0
-
-#                     if is_return:
-#                         item_amount = -abs(item_amount)
-
-#                     elif is_debit:
-#                         item_amount = abs(item_amount)
-
-#                     totals["Zero Rated"][key] += item_amount
-
-#         # EXPORTS
-#         if int(inv_doc.get("custom_zatca_export_invoice") or 0) == 1:
-
-#             totals["Exports"][key] += signed_grand_total
-
-#             if not has_item_template:
-#                 totals["Exports"]["vat"] += signed_vat
-
-#         # HEALTHCARE / EDUCATION
-#         if inv_doc.get("custom_exemption_reason_code") in [
-#             "VATEX-SA-HEA",
-#             "VATEX-SA-EDU"
-#         ]:
-
-#             totals["HealthcareEdu"][key] += signed_grand_total
-
-#             if not has_item_template:
-#                 totals["HealthcareEdu"]["vat"] += signed_vat
-
-#         else:
-
-#             for item in inv_doc["items"]:
-
-#                 if item.get("template_exemption_code") in [
-#                     "VATEX-SA-HEA",
-#                     "VATEX-SA-EDU"
-#                 ]:
-
-#                     item_amount = item.get("amount") or 0
-#                     item_vat = calculate_item_vat(item)
-
-#                     if is_return:
-#                         item_amount = -abs(item_amount)
-#                         item_vat = -abs(item_vat)
-
-#                     elif is_debit:
-#                         item_amount = abs(item_amount)
-#                         item_vat = abs(item_vat)
-
-#                     totals["HealthcareEdu"][key] += item_amount
-#                     totals["HealthcareEdu"]["vat"] += item_vat
-
-#         # EXEMPT
-#         if inv_doc.get("custom_zatca_tax_category") == "Exempted":
-
-#             totals["Exempt"][key] += signed_grand_total
-
-#         else:
-
-#             for item in inv_doc["items"]:
-
-#                 if item.get("template_category") == "Exempted":
-
-#                     item_amount = item.get("amount") or 0
-
-#                     if is_return:
-#                         item_amount = -abs(item_amount)
-
-#                     elif is_debit:
-#                         item_amount = abs(item_amount)
-
-#                     totals["Exempt"][key] += item_amount
-
-#     return totals
 
 # =========================================================
 # SALES VAT - FIXED VERSION
@@ -420,7 +187,7 @@ def get_sales_vat_totals_sql(filters):
 
     where_clause = build_filters_sql(filters)
 
-    query = f"""
+    query = """
         SELECT
             si.name AS invoice,
             si.is_return AS is_return,
@@ -462,8 +229,8 @@ def get_sales_vat_totals_sql(filters):
             ON tax.parent = itt.name
             AND tax.idx = 1
 
-        WHERE {where_clause}
-    """
+        WHERE
+    """ + where_clause
 
     rows = frappe.db.sql(query, filters, as_dict=True)
 
@@ -641,7 +408,7 @@ def get_purchase_vat_totals_sql(filters):
 
     where_clause = build_filters_sql(filters, table_alias="pi")
 
-    query = f"""
+    query = """
         SELECT
             pi.name AS invoice,
             pi.is_return AS is_return,
@@ -671,9 +438,8 @@ def get_purchase_vat_totals_sql(filters):
 
         FROM `tabPurchase Invoice` pi
 
-        WHERE {where_clause}
-    """
-
+        WHERE
+    """ + where_clause
     rows = frappe.db.sql(query, filters, as_dict=True)
 
     for r in rows:
@@ -739,7 +505,7 @@ def get_other_income_tax(filters):
 
     where_clause = " AND ".join(conditions)
 
-    query = f"""
+    query = """
         SELECT
             COALESCE(SUM(
                 CASE
@@ -762,11 +528,12 @@ def get_other_income_tax(filters):
         INNER JOIN `tabAccount` acc
             ON acc.name = gle.account
 
-        WHERE {where_clause}
-            AND acc.account_type = 'Tax'
-            AND IFNULL(acc.custom_zatca_account_type, '') IN ('Output Tax', 'Both')
-            AND gle.voucher_type NOT IN ('Sales Invoice', 'Purchase Invoice')
-            AND gle.is_cancelled = 0
+        WHERE
+    """ + where_clause + """
+        AND acc.account_type = 'Tax'
+        AND IFNULL(acc.custom_zatca_account_type, '') IN ('Output Tax', 'Both')
+        AND gle.voucher_type NOT IN ('Sales Invoice', 'Purchase Invoice')
+        AND gle.is_cancelled = 0
     """
 
     result = frappe.db.sql(query, filters, as_dict=True)
@@ -808,7 +575,8 @@ def get_other_expense_tax(filters):
 
     where_clause = " AND ".join(conditions)
 
-    query = f"""
+
+    query = """
         SELECT
             COALESCE(SUM(
                 CASE
@@ -831,15 +599,15 @@ def get_other_expense_tax(filters):
         INNER JOIN `tabAccount` acc
             ON acc.name = gle.account
 
-        WHERE {where_clause}
-            AND acc.account_type = 'Tax'
-            AND IFNULL(acc.custom_zatca_account_type, '') IN ('Input Tax', 'Both')
-            AND gle.voucher_type NOT IN ('Sales Invoice', 'Purchase Invoice')
-            AND gle.is_cancelled = 0
+        WHERE
+    """ + where_clause + """
+        AND acc.account_type = 'Tax'
+        AND IFNULL(acc.custom_zatca_account_type, '') IN ('Input Tax', 'Both')
+        AND gle.voucher_type NOT IN ('Sales Invoice', 'Purchase Invoice')
+        AND gle.is_cancelled = 0
     """
 
     result = frappe.db.sql(query, filters, as_dict=True)
-
     vat_amount = (
         result[0].get("vat_amount")
         if result and result[0].get("vat_amount")
